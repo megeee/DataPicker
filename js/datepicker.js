ab3a1049ce4d4cb603c0a2b4f33ea960e6dd0e79
JeffGuo <85876521@qq.com>
2017-04-16 23:49:25 +0800

    initinit

---
+++ js/datepicker.js
@@ -0,0 +1,17 @@
+;(function(win){
+    
+    var D =  function(){
+        console.log(this);
+    }
+
+    D.prototype.getDays = function(){
+
+    }
+
+    D.prototype.render = function(){
+
+    }
+
+    win.J_Datepicker =  new D();
+
+})(window)
